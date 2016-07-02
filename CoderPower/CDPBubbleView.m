c25fb1b638386ea1e28904b060f2d7a338a95d2e
tyzual <tyzual@tyzuals-MacBook-Pro.local>
2016-07-02 10:39:10 +0800

    add fade out animate

--- CoderPower/CDPBubbleView.m
+++ CoderPower/CDPBubbleView.m
@@ -72,11 +72,14 @@
 		CVDisplayLinkMgr *mgr = [CVDisplayLinkMgr getInstance];
 		[mgr addBubleAnimtateDelegate:self];
 
-		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		[NSAnimationContext runAnimationGroup:^(NSAnimationContext * _Nonnull context) {
+			context.duration = 0.5;
+			self.animator.alphaValue = 0;
+		} completionHandler:^{
 			[self removeFromSuperview];
 			CVDisplayLinkMgr *mgr = [CVDisplayLinkMgr getInstance];
 			[mgr deleteBubleAnimtateDelegate:self];
-		});
+		}];
 	}
 	return self;
 }
